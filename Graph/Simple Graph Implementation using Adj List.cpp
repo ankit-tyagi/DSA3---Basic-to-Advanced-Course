5adff20bc4800b44a99dbfed53bf94eb9aab8515
Ankit Tyagi <ankittyaagi@gmail.com>
2019-06-26 02:48:37 +0530

    Simple graph Implementation

--- Graph/Simple Graph Implementation using Adj List.cpp
+++ Graph/Simple Graph Implementation using Adj List.cpp
@@ -2,10 +2,38 @@
 
 using namespace std;
 
+void addedge(vector <int> adj[], int v, int u)
+{
+    adj[v].push_back(u);
+    adj[u].push_back(v);
+}
+
+void print(vector <int> adj[], int v)
+{
+    for(int i=0;i<v;i++)
+    {
+        cout << "\n Adjacency list of vertex "<<i<< "\n head "; 
+        for(vector <int>:: iterator x = adj[i].begin(); x!= adj[i].end();x++)
+            cout<<"->"<<*x;
+        cout<<endl;
+        
+    }
+}
+
 int main()
 {
     int v;
     cin>>v;
     vector<int> adj[v];
-    
+    int a,b,e;
+    cout<<"enter the number of edges";
+    cin>>e;
+    for(int i=0;i<e;i++)
+    {
+        cin>>a>>b;
+        addedge(adj,a,b);
+    }
+
+    print(adj,v);
+    return 0;
 }
