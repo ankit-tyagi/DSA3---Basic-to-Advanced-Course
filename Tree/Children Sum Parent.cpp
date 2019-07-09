4685e9f18ea91b1c7271440de848b621b22ddce6
Ankit Tyagi <ankittyaagi@gmail.com>
2019-07-10 04:28:25 +0530

    Children Sum Parent

---
+++ Tree/Children Sum Parent.cpp
@@ -0,0 +1,129 @@
+{
+/*input
+2
+3
+4 1 L 4 3 R 3 3 L
+5
+10 8 L 10 2 R 8 3 L 8 5 R 2 2 L
+*/
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+void insert(Node *root,int a1,int a2,char lr){
+	if(root==NULL){
+		return;
+	}
+	if(root->data==a1){
+		switch(lr){
+			case 'L':root->left=new Node(a2);
+			break;
+			case 'R':root->right=new Node(a2);
+			break;
+		}
+	}
+	else{
+		insert(root->left,a1,a2,lr);
+		insert(root->right,a1,a2,lr);
+	}
+}
+void inorder(Node *root){
+	if(root==NULL)
+		return;
+	inorder(root->left);
+	cout<<root->data<<" ";
+	inorder(root->right);
+}
+int isSumProperty(Node *node);
+int main()
+{
+	//freopen("input.txt","r",stdin);
+	//freopen("output.txt","w",stdout);
+	int t;
+	cin>>t;
+	while(t-->0)
+	{
+		int n;
+		cin>>n;
+		int m=n;
+		Node *root1=NULL;
+		while(n-->0){
+			int a1,a2;
+			cin>>a1>>a2;
+			char lr;
+			scanf(" %c",&lr);
+			if(root1==NULL){
+				root1=new Node(a1);
+				switch(lr){
+					case 'L':root1->left=new Node(a2);
+					break;
+					case 'R':root1->right=new Node(a2);
+					break;
+				}
+			}
+			else{
+				insert(root1,a1,a2,lr);
+			}
+		}
+			cout<<isSumProperty(root1)<<"
+";
+	}
+}
+
+}
+/*This is a function problem.You only need to complete the function given below*/
+/*Complete the function below
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+int isSumProperty(Node *node)
+{
+    if((node == NULL) || (node->left == NULL && node->right == NULL))
+        return 1;
+    int l=0,r=0;
+    
+    if(node->left != NULL)
+        l = node->left->data;
+    if(node->right != NULL)
+        r = node->right->data;
+    
+    return ((node->data == l+r) && isSumProperty(node->left) && isSumProperty(node->right));
+    
+    /*
+    if(!node || !node->left && !node->right)
+        return 1;
+    int ls=(node->left)?(node->left->data):0;
+    int rs=(node->right)?(node->right->data):0;
+    return ((node->data==(ls+rs))&& isSumProperty(node->left)&&isSumProperty(node->right));*/
+ // Add your code here
+}
+
+
+/*Input:
+2
+3
+4 1 L 4 3 R 3 3 L
+5
+10 8 L 10 2 R 8 3 L 8 5 R 2 2 L
+
+Output:
+1
+1 */
