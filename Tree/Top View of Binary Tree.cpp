921768d7ed41d46d60af7658d941ce10bda89534
Ankit Tyagi <ankittyaagi@gmail.com>
2019-07-10 04:01:35 +0530

    Top View of Binary Tree

---
+++ Tree/Top View of Binary Tree.cpp
@@ -0,0 +1,100 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+/* Computes the number of nodes in a tree. */
+int height(struct Node* node);
+void inorder(Node *root)
+{
+    if (root == NULL)
+       return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);    
+}
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d
+", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     struct Node *child;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = new Node(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = new Node(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     cout << height(root) << endl;
+  }
+  return 0;
+}
+
+
+/*This is a function problem.You only need to complete the function given below*/
+/* Tree node structure  used in the program
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+/* Computes the height of binary tree with given root.  */
+int height(Node* node)
+{
+    if(node == NULL)
+        return 0;
+    return (max(height(node->left),height(node->right))+1);
+   // Your code here
+}
+
+
+/*Input:
+2
+2
+1 2 L 1 3 R
+4
+10 20 L 10 30 R 20 40 L 20 60 R
+
+Output:
+2
+3 */
