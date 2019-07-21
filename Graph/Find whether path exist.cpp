516cd2365e57040e8e566ae7a32e43b33dfe6eeb
Ankit Tyagi <ankittyaagi@gmail.com>
2019-07-21 22:06:45 +0530

    Find whether path exist

---
+++ Graph/Find whether path exist.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+#define N 21
+
+bool isrechable(int a[][N], int n, int i, int j, int x, int y)
+{
+    if(i==x && j==y)
+        return true;
+    else if(a[i][j] == 3)
+        return false;
+    else if(i>=0 && i<n && j>=0 && j<n)
+        return(isrechable(a,n,i+1,j,x,y) || isrechable(a,n,i,j+1,x,y) || isrechable(a,n,i-1,j,x,y) || isrechable(a,n,i,j-1,x,y));
+    return false;
+    
+}
+
+using namespace std;
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n;
+         cin>>n;
+         int a[n][n];
+         int sx,sy,dx,dy;
+         for(int i=0;i<n;i++)
+         {
+             for(int j=0;j<n;j++)
+             {
+                 cin>>a[i][j];
+                 if(a[i][j] == 1)
+                 {
+                     sx = i;
+                     sy = j;
+                 }
+                 if(a[i][j] == 2)
+                 {
+                     dx = i;
+                     dy = j;
+                 }
+             }
+         }
+         if(isrechable(a,n,sx,sy,dx,dy))
+            cout<<"1"<<endl;
+        else
+            cout<<"0"<<endl;
+     }
+	//code
+	return 0;
+}
